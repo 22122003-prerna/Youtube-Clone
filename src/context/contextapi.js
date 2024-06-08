dc9e41f467db2a3ab8b062bb6d34a07480e8749c
Prerna <jainprerna156@gmail.com>
2024-06-08 21:18:58 +0530

    initial commit

---
+++ src/context/contextapi.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api.js";
+
+export const context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState(false);
+    const [selectCategories, setSelectCategories] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectCategories);
+    }, [selectCategories]);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then((res) => {
+            console.log(res);
+            // setSelectCategories(res)
+            setLoading(false);
+        });
+    };
+
+    return (
+        <context.Provider value={{
+            loading,
+            setLoading,
+            searchResults,
+            setSearchResults,
+            selectCategories,
+            setSelectCategories,
+            mobileMenu,
+            setMobileMenu,
+        }}>
+            {props.children}
+        </context.Provider>
+    );
+};
+
